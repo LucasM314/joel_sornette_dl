c6650dda011fad5fddfa20547af51ed5d2386bb8
Martiniano Lucas <lucas.martiniano@student-cs.fr>
2025-03-13 04:51:34 +0100

    Ajouter la possibilité de télécharger les archives de PC (exercices corrigés et cours)

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import requests
 from bs4 import BeautifulSoup
 
 
+#### Cours ####
+
 # Convertit la lettre du livre en son numéro correspondant
 def book_str_to_int(book):
     return ord(book) - 64
@@ -91,7 +93,6 @@ def find_title(book: str, chapter: int = -1) -> tuple[bool, str]:
     except requests.exceptions.RequestException as e:
         return False, e
 
-
 selection_complete = {'A': list(range(1, 11)),
                       'B': list(range(1, 23)),
                       'C': list(range(1, 14)),
@@ -111,6 +112,7 @@ def download_lessons(selection=None, folder_path=os.getcwd()):
         success_book_title, book_title = find_title(book)
         if not success_book_title:
             print(f"Erreur lors de l'obtention du titre du livre {book}")
+            continue
         # Evite les caractères spéciaux non supportés par Windows
         book_title = book_title.replace(':', '-').replace(" ?", "").replace("/", ", ")
         book_path = os.path.join(folder_path, book_title)
@@ -126,11 +128,13 @@ def download_lessons(selection=None, folder_path=os.getcwd()):
             success_find, url = find_latest_version_url(book, chapter)
             if not success_find:
                 print(f"Erreur lors de l'obtention du lien du chapitre {chapter} livre {book}")
+                continue
 
             # Récupère le nom du chapitre
             success_chapter_title, chapter_title = find_title(book, chapter)
             if not success_chapter_title:
                 print(f"Erreur lors de l'obtention du titre du chapitre {chapter} livre {book}")
+                continue
             # Dans certains cas, le titre du livre est de la forme "A blabla" au lieu de "A : blabla".
             chapter_title = chapter_title.replace(': ', '')
             chapter_id, chapter_name = chapter_title.split(' ', 1)
@@ -143,9 +147,165 @@ def download_lessons(selection=None, folder_path=os.getcwd()):
                 print(f"Erreur lors du téléchargement du chapitre {chapter} livre {book}")
 
 
+#### Archives ####
+
+# L'encodage apparent de la page web est ISO-8859-1, mais le texte est en UTF-8.  
+# Cette fonction corrige l'encodage.
+def fix_encoding(text):
+    try:
+        return text.encode("latin1").decode("utf-8")
+    except UnicodeEncodeError:
+        return text  # Si ça échoue, on garde le texte original
+
+# Renvoie la liste des archives d'un thème (principal ou secondaire).
+def get_topic_archives(topic_list):
+    archives_list = []
+    for archive_a in topic_list.find_all("a"):
+        archive_name = fix_encoding(archive_a.get_text(strip=True))
+        # Mettre la première lettre en majuscule
+        formatted_name = archive_name[0].upper() + archive_name[1:]
+        archive_url = r"https://www.joelsornette.fr/Archives/" + archive_a["href"]
+        archives_list.append((formatted_name, archive_url))
+    return archives_list
+
+def find_archives(archive_type='e'):
+    """
+    Télécharge et extrait la liste des exos/cours archivés.  
+
+    Fonctionnement :
+    -------------
+    La structure générale des pages d'archive exos/cours est la suivante :
+    <ul>
+    <li><span><b><i>Thème 1</i></b></span>
+        <ul>
+        <li><span>Sous-thème 1</span>
+            <ul>
+                <li><a href="URL">Nom de l'exercice</a>
+                <li><a href="URL">Nom de l'exercice</a>
+            </ul>
+        <li><span>Sous-thème 2</span>
+            <ul>
+                <li><a href="URL">Nom de l'exercice</a>
+                <li><a href="URL">Nom de l'exercice</a>
+            </ul>
+        </ul>
+    
+    <li><span>Thème 2</span>
+       etc.
+    
+    La manière la plus simple de récupérer les archives est de parcourir les balises <li> imbriquées les unes dans les autres.
+    Toutefois, il semble que BeautifoulSoup ait du mal avec celles-ci car elles ne sont pas fermées.
+    Une solution alternative est de récupérer les balises <b> qui ne contiennent que les thèmes principaux,
+    puis de trouver les balises <span> qui contiennent les sous-thèmes, et enfin de récupérer les balises 
+    <a> qui contiennent les archives, en avançant dans le document à partir des balises <span> grâce à find_next.
+
+    Renvoie :
+    ----------
+    tuple :
+        - (bool) Indique si l'extraction a réussi (True) ou échoué (False).
+        - (dict | Exception) Si succès, retourne un dictionnaire structuré contenant les archives.
+          En cas d'échec, retourne l'exception rencontrée.
+
+    Structure du dictionnaire retourné :
+    ------------------------------------
+    {
+        "Nom du thème principal": {
+            "Nom du sous-thème": [
+                ("Nom de l'archive", "URL de l'archive"),
+                ...
+            ]
+        }
+    }
+    """
+    url = f"https://www.joelsornette.fr/Archives/{"ExercicesCorriges" if archive_type=='e' else "Cours"}.html"
+    try:
+        response = requests.get(url, timeout=10)
+
+        if response.status_code != 200:  # Requête échouée
+            print(f"Erreur lors de l'obtention de la page {url}")
+            return
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+        
+        # Dictionnaire pour stocker les archives
+        archives = {}
+
+        # Parcourir les thèmes principaux, qui sont les seuls en gras
+        for main_topic_b in soup.select("span > b"):
+            main_topic = fix_encoding(main_topic_b.get_text(strip=True))
+            main_topic_li = main_topic_b.find_next("ul")
+            archives[main_topic] = {}
+            
+            # Parcourir les sous-thèmes, qui sont les seuls spans à l'intérieur des thèmes principaux
+            # Si aucun sous-thème n'est trouvé, les archives sont placés dans un sous-thème vide.
+            subtopics = main_topic_li.find_all("span")
+            if subtopics:
+                for subtopic_span in main_topic_li.find_all("span"):
+                    subtopic = fix_encoding(subtopic_span.get_text(strip=True))
+                    subtopic_li = subtopic_span.find_next("ul")
+                    archives[main_topic][subtopic] = get_topic_archives(subtopic_li)
+            else:
+                if "" not in archives[main_topic].keys():
+                    archives[main_topic][""] = []
+                archives[main_topic][""] += get_topic_archives(main_topic_li)
+        
+        return True, archives
+
+    except requests.exceptions.RequestException as e:
+        return False, e
+    
+def download_archives(folder_path=os.getcwd(), archive_type='e'):
+    """
+    Télécharge et enregistre les archives du type choisi (exercices ou cours).
+
+    Paramètres :
+    ------------
+    folder_path : str, optionnel
+        Chemin du dossier où enregistrer les fichiers. Par défaut, le dossier de travail actuel.
+    archive_type : str
+        Type d'archives à télécharger. 'e' pour les exercices et 'p' pour les cours.
+    """
+    success, archives = find_archives(archive_type)
+    if not success:
+        print(f"Erreur lors de l'obtention du titre {"des exercices" if archive_type=='e' else "du cours"} : {archives}")
+        return
+
+    for main_topic, subtopics in archives.items():
+        for subtopic, topic_archives in subtopics.items():
+            # Créer les dossiers correspondants
+            subtopic_path = os.path.join(folder_path, main_topic.capitalize(), subtopic.capitalize())
+            os.makedirs(subtopic_path, exist_ok=True)
+
+            for archive_name, archive_url in topic_archives:
+                pdf_filename = f"{archive_name}.pdf".replace(":", "-").replace("/", "-").replace("\\", "-").replace("?", "").replace('"', "'")
+                pdf_path = os.path.join(subtopic_path, pdf_filename)
+                success_download = download_pdf(archive_url, pdf_path)
+                if not success_download:
+                    print(f"Erreur lors du téléchargement {"de l'exercice" if archive_type=='e' else "du cours"} : {archive_name}")
+
+
+#### Main ####
+
+def usage_message():
+    print("Utilisation : python main.py [c | e | p] dossier_de_destination")
+    print("Options :")
+    print("  c -> Cours")
+    print("  e -> Exercices d'archives de PC")
+    print("  p -> Cours d'archives de PC")
+    print("\nExemple : python main.py c mon_dossier")
+
 if __name__ == '__main__':
-    if len(os.sys.argv) != 2:
-        print("Input the folder_path for the lessons to be downloaded in")
+    if len(os.sys.argv) != 3:
+        usage_message()
         os.sys.exit()
     
-    download_lessons(folder_path=os.sys.argv[1])
+    dl_type = os.sys.argv[1][0].lower()
+    if dl_type == 'c':
+        download_lessons(folder_path=os.sys.argv[2])
+    elif dl_type == 'e':
+        download_archives(os.sys.argv[2], 'e')
+    elif dl_type == 'p':
+        download_archives(os.sys.argv[2], 'p')
+    else:
+        usage_message()
+        os.sys.exit()
